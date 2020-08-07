ddd3b07026d6e02898d28322d70e486d37847e71
Foo-x <www.sand.x@gmail.com>
2020-08-07 20:36:10 +0900

    Add some algorithm utils

--- src/contestlib/calgorithm.nim
+++ src/contestlib/calgorithm.nim
@@ -49,3 +49,32 @@ iterator permutations*[T](a: var openArray[T]): seq[T] =
   yield v
   while v.nextPermutation():
     yield v
+
+iterator reversedIter*[T](a: openArray[T]): T =
+  for i in countdown(a.len - 1, 0):
+    yield a[i]
+
+iterator reversedPairs*[T](a: openArray[T]): tuple[key: int, val: T] =
+  for i in countdown(a.len - 1, 0):
+    yield (i, a[i])
+
+proc findLast*[T](a: openArray[T], item: T): int =
+  for i, x in a.reversedPairs:
+    if x == item:
+      return i
+
+  return -1
+
+proc findIf*[T](a: openArray[T], pred: proc (x: T): bool): int =
+  for i, x in a.pairs:
+    if pred(x):
+      return i
+
+  return -1
+
+proc findLastIf*[T](a: openArray[T], pred: proc (x: T): bool): int =
+  for i, x in a.reversedPairs:
+    if pred(x):
+      return i
+
+  return -1
