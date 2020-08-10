a0471d09a1f39521a7d73a390e2f35a8a865a12d
Foo-x <www.sand.x@gmail.com>
2020-08-10 16:54:32 +0900

    Add bitops import

--- src/contestlib/imports.nim
+++ src/contestlib/imports.nim
@@ -1,2 +1,3 @@
 import strformat, macros, algorithm, tables, sets, lists,
-    intsets, critbits, sequtils, strutils, math, times, sugar, options, deques
+    intsets, critbits, sequtils, strutils, math, times,
+    sugar, options, deques, bitops
