b2a91f0749cfa54901de1ffa3cc3edd9930b4936
Foo-x <www.sand.x@gmail.com>
2020-08-10 16:45:23 +0900

    Add from/to binseq

--- src/contestlib/cbitset.nim
+++ src/contestlib/cbitset.nim
@@ -1,4 +1,4 @@
-import sequtils
+import algorithm, sequtils, bitops
 
 proc bitLen(t: typedesc): int =
   sizeof(t) * 8 - 1
@@ -19,7 +19,7 @@ proc `[]`*(a: BitSet, i: BitsRange[typeof a]): bool =
 proc toBoolSeq*(a: BitSet): seq[bool] =
   (0..bitLen(typeof a)).mapIt(a[it])
 
-proc fromBoolSeq*(a: seq[bool]): BitSet =
+proc fromBoolSeq*(a: openArray[bool]): BitSet =
   result = 0
   for i in 0..<a.len:
     result[i] = a[i]
@@ -27,7 +27,44 @@ proc fromBoolSeq*(a: seq[bool]): BitSet =
 proc keys*(a: BitSet): seq[int] =
   (0..bitLen(typeof a)).toSeq.filterIt(a[it])
 
-proc fromKeys*(keys: seq[int]): BitSet =
+proc fromKeys*(keys: openArray[int]): BitSet =
   result = 0
   for k in keys:
     result[k] = true
+
+proc toBinSeq*(a: BitSet, maxLen: range[0..bitLen(int)+1] = 0): seq[int] =
+  ## 2進数のseqに変換します。
+  ## `maxLen`が0の場合、`BitSet`を表す最短の長さで返します。
+  runnableExamples:
+    doAssert 29.toBinSeq() == @[1, 1, 1, 0, 1]
+    doAssert 29.toBinSeq(3) == @[1, 0, 1]
+    doAssert 29.toBinSeq(10) == @[0, 0, 0, 0, 0, 1, 1, 1, 0, 1]
+
+  if maxLen == 0:
+    var i = 0
+    while 1 shl i <= a:
+      result.insert(if a.testBit i: 1 else: 0, 0)
+      i += 1
+
+  else:
+    result = newSeq[int](maxLen)
+    for i in 0..<maxLen:
+      result[^(i+1)] = if a.testBit i: 1 else: 0
+
+proc fromBinSeq*(bs: openArray[int]): BitSet =
+  ## 2進数のseqから`BitSet`に変換します。
+  ## 2進数以外の値が含まれていた場合は0を返します。
+  runnableExamples:
+    doAssert @[1, 1, 1, 0, 1].fromBinSeq() == 29
+    doAssert @[0, 0, 0, 0, 0, 1, 1, 1, 0, 1].fromBinSeq() == 29
+    doAssert @[1, 1, 2, 0, 1].fromBinSeq() == 0
+
+  if bs.filterIt(it != 0 and it != 1).len > 0:
+    return 0
+  if bs.find(1) == -1:
+    return 0
+
+  result = 0
+  for i in bs.reversed.find(1)..<(bs.len - bs.find(1)):
+    if bs[^(i+1)] == 1:
+      result += 1 shl i
