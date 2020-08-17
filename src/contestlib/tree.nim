0aa0496ff68925d45fa6aa4268dfb3efb4e7a068
Foo-x <www.sand.x@gmail.com>
2020-08-17 23:34:20 +0900

    Add kruskal

--- src/contestlib/tree.nim
+++ src/contestlib/tree.nim
@@ -1,4 +1,4 @@
-import tree/bit
+import tree/[bit, unionfind]
 import algorithm
 
 proc invCount*[T](s: openArray[T]): int =
@@ -18,3 +18,17 @@ proc invCountWithLen*[T](s: openArray[T]): int =
     var index = sortedS.find(s[i])
     result += i - bit.sum0(index)
     bit.add0(index, 1)
+
+type
+  Edge* = tuple[src, dst, cost: int]
+
+proc kruskal*(edges: seq[Edge], maxN: int): int =
+  ## 最小全域木の和を返します。
+  ## クラスカル法: O(ElogV)
+  var uf = newUnionFind(maxN)
+  for e in edges.sortedByIt(it.cost):
+    if uf.isSame(e.src, e.dst):
+      continue
+
+    uf.merge(e.src, e.dst)
+    result = max(result, e.cost)
